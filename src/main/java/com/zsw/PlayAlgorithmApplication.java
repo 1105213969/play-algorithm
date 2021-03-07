a0efa599fb569cd1a0b4370401c11d69b434acd5
1105213969 <1105213969@qq.com>
2021-03-07 21:53:49 +0800

    初始化项目

---
+++ src/main/java/com/zsw/PlayAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.zsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zsw/PlayAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
