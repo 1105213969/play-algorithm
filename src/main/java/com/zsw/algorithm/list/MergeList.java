64d9ae3e1c8e7e53844ca33980b1f9d6bf1ec82d
1105213969 <1105213969@qq.com>
2021-04-06 23:17:58 +0800

    合并链表

---
+++ src/main/java/com/zsw/algorithm/list/MergeList.java
@@ -0,0 +1,24 @@
+package com.zsw.algorithm.list;
+
+import com.zsw.algorithm.ListNode;
+
+/**
+ * 合并链表
+ * @author zsw
+ *
+ */
+public class MergeList {
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l1 == null) return l2;
+	    if (l2 == null) return l1;
+
+	    if (l1.val < l2.val) {
+	    	l1.next = mergeTwoLists(l1.next, l2);
+	        return l1;
+	    } else {
+	        l2.next = mergeTwoLists(l1, l2.next);
+	        return l2;
+	    }
+	 }
+}
