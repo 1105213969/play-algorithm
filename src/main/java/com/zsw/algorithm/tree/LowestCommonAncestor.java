73c08197ccc0cb9fbf0ab533e9fe89887b86867d
1105213969 <1105213969@qq.com>
2021-04-25 23:20:49 +0800

    二叉树最近公共祖先

---
+++ src/main/java/com/zsw/algorithm/tree/LowestCommonAncestor.java
@@ -0,0 +1,31 @@
+package com.zsw.algorithm.tree;
+
+import com.zsw.algorithm.TreeNode;
+
+/**
+ * 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null && right != null) {
+            //p,q在右子树上
+            return right;
+        }
+        if (left != null && right == null) {
+            //p,q在左子树上
+            return left;
+        }
+        if (left == null && right == null) {
+            //没有公共祖先
+            return null;
+        }
+        return root;
+    }
+}
