ea936ea0c7954af572838df8be616d7368083886
1105213969 <1105213969@qq.com>
2021-05-29 09:16:31 +0800

    fomat code

--- src/main/java/com/zsw/algorithm/arrary/TopKFrequent.java
+++ src/main/java/com/zsw/algorithm/arrary/TopKFrequent.java
@@ -12,37 +12,33 @@ public class TopKFrequent {
      */
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> count = new HashMap<>();
-        for(int num : nums){
+        for (int num : nums) {
             count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] m, int[] n){
-                return m[1] - n[1];
-            }
-        });
+        PriorityQueue<int[]> queue = new PriorityQueue<>((m, n) -> m[1] - n[1]);
 
-        for(Map.Entry<Integer, Integer> entry : count.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
             int num = entry.getKey(), cnt = entry.getValue();
-            if(queue.size() == k){
-                if(cnt > queue.peek()[1]){
+            if (queue.size() == k) {
+                if (cnt > queue.peek()[1]) {
                     queue.poll();
                     queue.offer(new int[]{num, cnt});
                 }
-            }else{
+            } else {
                 queue.offer(new int[]{num, cnt});
             }
         }
 
         int[] res = new int[k];
-        for(int i = 0; i < k; i++){
+        for (int i = 0; i < k; i++) {
             res[i] = queue.poll()[0];
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,3,1,1,1,3,73,1};
+        int[] arr = {5, 3, 1, 1, 1, 3, 73, 1};
         new TopKFrequent().topKFrequent(arr, 2);
     }
 }
