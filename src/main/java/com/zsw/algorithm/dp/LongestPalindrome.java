66ad3178dd28206a0a1e7fb02f89f69da78fce78
1105213969 <1105213969@qq.com>
2021-05-29 17:46:41 +0800

    最长回文子串

--- src/main/java/com/zsw/algorithm/dp/LongestPalindrome.java
+++ src/main/java/com/zsw/algorithm/dp/LongestPalindrome.java
@@ -6,38 +6,32 @@ package com.zsw.algorithm.dp;
 public class LongestPalindrome {
 
     public String longestPalindrome(String s) {
-        if (s.length() < 2) {
+         //dp[i][j] s[i...j]是回文子串
+         if (s.length() < 2) {
             return s;
         }
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
+        StringBuilder sb = new StringBuilder(s);
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < s.length(); ++i) {
             dp[i][i] = true;
         }
-
         int maxLen = 1;
-        int begin = 0;
-        for (int j = 1; j < n; ++j) {
-            for (int i = 0; i < j; i++) {
-                if (s.charAt(i) != s.charAt(j)) {
-                    dp[i][j] = false;
-                } else {
-                    if (j - i < 3) {
+        int start = 0;
+        for (int j = 0; j < s.length(); ++j) {
+            for (int i = 0; i < j; ++i) {
+                if (sb.charAt(i) == sb.charAt(j)) {
+                    if (j - i < 3 || dp[i + 1][j - 1]) {
                         dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
+
+                        if (j - i + 1 > maxLen) {
+                            maxLen = j- i + 1;
+                            start = i;
+                        }
                     }
-                }
-
-
-                if (dp[i][j] && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    //更新最大子串的初始位置
-                    begin = i;
                 }
             }
         }
 
-        return s.substring(begin, begin + maxLen);
+        return sb.substring(start, start + maxLen);
     }
 }
